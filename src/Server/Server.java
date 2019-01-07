28dfc178d80f37befcccebf5b46a13b43adaa004
Alexandre <schweizer.alexandre@gmail.com>
2019-01-07 18:52:05 +0100

    Server update

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -3,9 +3,24 @@ package Server;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+import Client.Client;
 
 public class Server {
 	public static void main(String[] args) {
+		
+		List<Client> clients = new ArrayList<Client>() ;
+		
+		for (int i = 0; i < clients.size(); i++) {
+			clients.get(i).getNom() ;
+			clients.get(i).getPrenom() ;
+			clients.get(i).getIpAddress() ;
+			clients.get(i).getUserId() ;
+			clients.get(i).getPassword() ;
+			clients.get(i).getFichers() ;
+		}
 
 		ServerSocket srvSocket = null ;
 		try {
